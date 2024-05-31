ffc152f22bb659004612003cf2b64f97391ecdb7
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-05-31 10:13:05 -0600

    added banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Demo from "./components/Demo/Demo";
 import HomeHeader from "./components/Home/HomeHeader";
 import DemoHeader from "./components/Demo/DemoHeader";
+import { FaGalacticSenate } from "react-icons/fa6";
 
 function App() {
-  const auth = false;
+  const currentUser = FaGalacticSenate;
   return (
     <>
-    {auth ? <HomeHeader/> : <DemoHeader />}
+      {currentUser ? <HomeHeader /> : <DemoHeader />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/demo" element={<Demo />} />
+        {currentUser && <Route path="/" element={<Home />} />}
+        {!currentUser && <Route path="/demo" element={<Demo />} />}
+        {/* route protection */}
+        <Route
+          path="*"
+          element={<Navigate to={!currentUser ? "/demo" : "/"} />}
+        />
       </Routes>
     </>
   );
