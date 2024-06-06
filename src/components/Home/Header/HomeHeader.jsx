a0669293437a125ac4aeaf586f76d9f5392bc586
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-06-05 18:35:14 -0600

    added user modal to project

--- src/components/Home/Header/HomeHeader.jsx
+++ src/components/Home/Header/HomeHeader.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsMedium } from "react-icons/bs";
 import { CiSearch } from "react-icons/ci";
 import { LiaEditSolid } from "react-icons/lia";
 import { IoMdNotificationsOutline } from "react-icons/io";
-import { MdKeyboard } from "react-icons/md";
+import { MdKeyboard, MdKeyboardArrowDown } from "react-icons/md";
 import { Link } from "react-router-dom";
 import Search from "./Search";
+import Modal from "../../../utils/Modal";
+import UserModal from "./UserModal";
 
 const HomeHeader = () => {
+  const [modal, setModal] = useState(false);
   return (
     <header className="border-b border-gray-200">
       <div className="size h-[60px] flex items-center justify-between">
@@ -31,6 +34,25 @@ const HomeHeader = () => {
             </span>
             <span className="text-sm mt-2">Write</span>
           </Link>
+          <span className="text-3xl text-gray-500 cursor-pointer">
+            <IoMdNotificationsOutline />
+          </span>
+          <div className="flex item-center relative">
+            <img
+              onClick={() => setModal(true)}
+              className="w-[2.3rem] hr-[2.3rem] object-cover rounded-full cursor-pointer"
+              src="/blank-profile.png"
+              alt="profile image"
+            />
+            <span className="text-gray-500 cursor-pointer">
+              <MdKeyboardArrowDown />
+            </span>
+            <Modal modal={modal} setModal={setModal} hidden={modal ? "" : "hidden"}>
+              <div className={`${modal ? "visible opacity-100%" : "invisible opacity-0"} transition-all duration-100`}>
+                <UserModal />
+              </div>
+            </Modal>
+          </div>
         </div>
       </div>
     </header>
